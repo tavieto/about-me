fb1594d538734ca0b6a3473ca14f5f587f49b39c
Otávio Moreira <tavieto@outlook.com>
2021-06-17 21:37:16 -0300

    update MainActivity

--- app/src/main/java/com/tavieto/aboutme/MainActivity.kt
+++ app/src/main/java/com/tavieto/aboutme/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.tavieto.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.tavieto.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.nicknameText.setOnClickListener {
-            updateNickname(it)
+            updateNickname(binding.nicknameEdit)
         }
     }
 
@@ -42,5 +43,19 @@ class MainActivity : AppCompatActivity() {
         binding.nicknameText.visibility = View.GONE
 
         binding.nicknameEdit.requestFocus()
+
+        // Show the keyboard
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(view, 0)
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        return when (keyCode) {
+            KeyEvent.KEYCODE_ENTER -> {
+                addNickname(binding.button)
+                true
+            }
+            else -> super.onKeyUp(keyCode, event)
+        }
     }
 }
